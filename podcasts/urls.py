8a15d7a9665ee91bf8cbc96c1ccc095a073b1beb
dkotschessa <itzdavey@gmail.com>
2023-04-12 07:55:34 -0400

    podcast info page

--- podcasts/urls.py
+++ podcasts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, podcast_info_view
 
 urlpatterns = [
-    path("", HomePageView.as_view(), name = "homepage")
+    path("", HomePageView.as_view(), name = "homepage"),
+    path("podcast/<int:podcast_id>", podcast_info_view, name="podcast_info")
 ]
--- podcasts/views.py
+++ podcasts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from .models import Episode
+from .models import Episode, Podcast
 
 class HomePageView(ListView):
     template_name = "homepage.html"
@@ -12,6 +12,7 @@ class HomePageView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         episodes = Episode.objects.filter().order_by("-pub_date")[:50]
+        podcast_id = episodes[0].podcast_name_id
         episode_list = []
         for episode in episodes:
             context_dict = {}
@@ -20,6 +21,28 @@ class HomePageView(ListView):
             context_dict["podcast_title"] = episode.title
             context_dict["episode_description"] = episode.description
             context_dict["episode_link"] = episode.link
+            context_dict["podcast_id"] = podcast_id
             episode_list.append(context_dict)
         context["episodes"] = episode_list
         return context
+
+
+
+
+
+def podcast_info_view(request, podcast_id):
+    podcast = Podcast.objects.get(pk=podcast_id)
+
+    return render(
+        request, "podcast_info.html", {"podcast" : podcast}
+    )
+        #
+        #
+        # context = {}
+        # context['podcast_href'] = podcast.feed_href
+        # context['podcast_name'] = podcast.podcast_name
+        # context['podcast_summary'] = podcast.podcast_summary
+        # context['podcast_image'] = podcast.podcast_image
+        # return context
+
+
