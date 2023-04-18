4f957e14aeae76854a341cc4879fc36c01463f27
David Kotschessa <itzdavey@gmail.com>
2023-04-18 04:53:22 -0400

    updated templates and views

--- podcasts/views.py
+++ podcasts/views.py
@@ -2,32 +2,24 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Episode, Podcast
 
-class HomePageView(ListView):
-    template_name = "homepage.html"
-    model = Episode
 
-
-     
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        episodes = Episode.objects.filter().order_by("-pub_date")[:50]
-        podcast_id = episodes[0].podcast_name_id
-        episode_list = []
-        for episode in episodes:
-            context_dict = {}
-            context_dict["episode_image"] = episode.image
-            context_dict["podcast_name"] = episode.podcast_name
-            context_dict["podcast_title"] = episode.title
-            context_dict["episode_description"] = episode.description
-            context_dict["episode_link"] = episode.link
-            context_dict["podcast_id"] = podcast_id
-            episode_list.append(context_dict)
-        context["episodes"] = episode_list
-        return context
-
-
-
+def homepage_view(request):
+    episodes = Episode.objects.filter().order_by("-pub_date")[:20]
+    #TODO one of each
+    podcast_id = episodes[0].podcast_name_id
+    episode_list = []
+    context = {}
+    for episode in episodes:
+        context_dict = {}
+        context_dict["episode_image"] = episode.image
+        context_dict["podcast_name"] = episode.podcast_name
+        context_dict["podcast_title"] = episode.title
+        context_dict["episode_description"] = episode.description
+        context_dict["episode_link"] = episode.link
+        context_dict["podcast_id"] = podcast_id
+        episode_list.append(context_dict)
+    context["episodes"] = episode_list
+    return render(request, "homepage.html", context)
 
 
 def podcast_info_view(request, podcast_id):
