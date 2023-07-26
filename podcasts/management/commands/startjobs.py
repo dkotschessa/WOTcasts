cfa93da415c490d0027d396283cd374596cff21b
David Kotschessa <itzdavey@gmail.com>
2023-07-26 08:53:09 -0400

    ability to run startjobs at any interval

--- podcasts/management/commands/startjobs.py
+++ podcasts/management/commands/startjobs.py
@@ -21,19 +21,24 @@ def delete_old_job_executions(max_age = 604_800):
 
 
 class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument('interval', type=int, help='Indicates number of minutes to wait between fetching')
+
     def handle(self, *args, **options):
         scheduler = BlockingScheduler(timezone = settings.TIME_ZONE)
         scheduler.add_jobstore(DjangoJobStore(), "default")
+        interval=options['interval']
 
         scheduler.add_job(
             fetch_new_episodes,
             trigger = "interval",
-            minutes = 2,
+            minutes = interval,
             id = "Fetch New Podcast Episodes",
             max_instances = 1,
             replace_existing=True
         )
-        logger.info("Added job: Fetch new episodes")
+        logger.info(f"Added job: Fetch new episodes every {interval} minutes")
 
         try:
             logger.info("Starting Scheduler")
