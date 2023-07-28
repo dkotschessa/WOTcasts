06fb4a733f3ad0d9bcf08312716c7de298aa1195
David Kotschessa <itzdavey@gmail.com>
2023-07-28 11:34:40 -0400

    database settings using environmental variables

--- content_aggregator/settings/base.py
+++ content_aggregator/settings/base.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -92,23 +95,27 @@ WSGI_APPLICATION = 'content_aggregator.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+# keep this for local setting
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+#
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "OPTIONS": {
-            "service": "my_service",
-            "passfile": ".my_pgpass",
-        },
-    }
+    "default" :
+        {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": str(os.getenv("DATABASE_NAME")),
+            "USER" : str(os.getenv("DATABASE_USER")),
+            "PASSWORD": str(os.getenv("DATABASE_PASS")),
+            "HOST": str(os.getenv("DATABASE_HOST")),
+            "PORT": str(os.getenv("DATABASE_PORT"))
+        }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
