5fc2606e1f67f1c3652ea1902c755ba1cdd9fc9b
David Kotschessa <itzdavey@gmail.com>
2023-07-29 05:28:51 -0400

    fixed issue in podast view

--- podcasts/views.py
+++ podcasts/views.py
@@ -6,7 +6,6 @@ from .models import Episode, Podcast
 def homepage_view(request):
     episodes = Episode.objects.filter().order_by("-pub_date")[:20]
     #TODO one of each
-    podcast_id = episodes[0].podcast_name_id
     episode_list = []
     context = {}
     for episode in episodes:
@@ -16,7 +15,7 @@ def homepage_view(request):
         context_dict["podcast_title"] = episode.title
         context_dict["episode_description"] = episode.description
         context_dict["episode_link"] = episode.link
-        context_dict["podcast_id"] = podcast_id
+        context_dict["podcast_id"] = episode.podcast_name_id
         episode_list.append(context_dict)
     context["episodes"] = episode_list
     return render(request, "podcasts/homepage.html", context)
@@ -28,4 +27,5 @@ def podcast_info_view(request, podcast_id):
     return render(
         request, "podcasts/podcast_info.html", {"podcast" : podcast}
     )
+
 
