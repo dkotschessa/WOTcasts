3f0cff7b4450bcfe6f5d1564025553551d9f0074
David Kotschessa <itzdavey@gmail.com>
2023-07-29 21:23:03 -0400

    various UI and views

--- podcasts/urls.py
+++ podcasts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import homepage_view, podcast_info_view
+from .views import homepage_view, podcast_info_view, podcast_gallery_view
 
 urlpatterns = [
     path("", homepage_view, name = "homepage"),
+    path('podcasts', podcast_gallery_view, name="podcast_gallery"),
     path("podcast/<int:podcast_id>", podcast_info_view, name="podcast_info")
 ]
--- podcasts/views.py
+++ podcasts/views.py
@@ -20,12 +20,43 @@ def homepage_view(request):
     context["episodes"] = episode_list
     return render(request, "podcasts/homepage.html", context)
 
-
 def podcast_info_view(request, podcast_id):
     podcast = Podcast.objects.get(pk=podcast_id)
-
+    episodes = Episode.objects.filter(podcast_name_id=podcast.id).order_by("-pub_date")[:20]
+    episode_list = []
+    context = {"podcast" : podcast}
+    for episode in episodes:
+        context_dict = {}
+        context_dict["episode_image"] = episode.image
+        context_dict["podcast_name"] = episode.podcast_name
+        context_dict["podcast_title"] = episode.title
+        context_dict["episode_description"] = episode.description
+        context_dict["episode_link"] = episode.link
+        context_dict["podcast_id"] = episode.podcast_name_id
+        episode_list.append(context_dict)
+    context["episodes"] = episode_list
     return render(
-        request, "podcasts/podcast_info.html", {"podcast" : podcast}
+        request, "podcasts/podcast_info.html", context
     )
+
+
+
+def podcast_gallery_view(request):
+    podcasts = Podcast.objects.all()
+    podcast_list = []
+    context = {}
+    for podcast in podcasts:
+        context_dict = {}
+        context_dict['podcast_name'] = podcast.podcast_name
+        context_dict['podcast_summary'] = podcast.podcast_summary
+        context_dict['podcast_image'] = podcast.podcast_image
+        context_dict["podcast_id"] = podcast.id
+        podcast_list.append(context_dict)
+
+    context['podcasts'] = podcast_list
+    return render(request, "podcasts/podcast_gallery.html", context)
+
+
+
 
 
