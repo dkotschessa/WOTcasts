876b9ce5bb9d59955a70d0360db54b9e6f0bad50
David Kotschessa <itzdavey@gmail.com>
2023-08-04 12:37:58 -0400

    more modifications to test

--- runtests.sh
+++ runtests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Running all tests and saving coverage report"
-pytest -v --cov=.  --cov-report=html podcasts accounts
-open htmlcov/index.html
+pytest -v --html=testreportdata/pytest_report.html  --self-contained-html --cov=.  --cov-report=html podcasts accounts
+open testreportdata/report.html
