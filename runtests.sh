ba100d4beda5ebe14a1332c1a4b5a5f26ecb3ffd
David Kotschessa <itzdavey@gmail.com>
2023-08-04 12:49:05 -0400

    ok that open html thing is annoying

--- runtests.sh
+++ runtests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Running all tests and saving coverage report"
 pytest -v --html=testreportdata/pytest_report.html  --self-contained-html --cov=.  --cov-report=html podcasts accounts
-open testreportdata/report.html
+echo report in testreportdata/report.html
