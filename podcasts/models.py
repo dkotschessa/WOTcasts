752fdcba877bafbac1e310de6f21241daec53332
David Kotschessa <itzdavey@gmail.com>
2023-08-13 19:29:19 -0400

    updates to parsers and adding youtube

--- podcasts/models.py
+++ podcasts/models.py
@@ -25,3 +25,25 @@ class Episode(models.Model):
 
     def __str__(self) -> str:
         return f"{self.podcast_name}: {self.title}"
+
+
+class Channel(models.Model):
+    feed_href = models.URLField(unique=True)
+    channel_name = models.CharField(max_length=100)
+    channel_summary = models.TextField(null=True, blank=True)
+    channel_image = models.URLField()
+    host = models.TextField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.channel_name}"
+
+
+class YoutubeEpisode(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    pub_date = models.DateTimeField()
+    link = models.URLField()
+    image = models.URLField()
+    podcast_name = models.ForeignKey(Podcast, on_delete=models.CASCADE)
+    guid = models.CharField(max_length=200)
+    duration = models.CharField(max_length=10, blank=True, null=True)
