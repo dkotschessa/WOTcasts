a914e27c0294039437089fdefdfc6a599244c3c9
David Kotschessa <itzdavey@gmail.com>
2023-08-22 09:02:25 -0400

    settings and env changes

--- content_aggregator/settings/base.py
+++ content_aggregator/settings/base.py
@@ -10,11 +10,17 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import environ
 from pathlib import Path
 import os
 from dotenv import load_dotenv
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 load_dotenv()
 # SENTRY
@@ -41,10 +47,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = str(os.getenv("DJANGO_SECRET_KEY"))
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
