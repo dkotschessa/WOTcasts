816e22afa6baecdcc6be5d7f3ef89048e9e2e774
David Kotschessa <itzdavey@gmail.com>
2023-08-22 10:31:59 -0400

    migrations

--- podcasts/models.py
+++ podcasts/models.py
@@ -6,6 +6,7 @@ class Podcast(models.Model):
     podcast_name = models.CharField(max_length=100)
     podcast_summary = models.TextField(null=True, blank=True)
     podcast_image = models.URLField()
+    podcast_twitter = models.URLField(unique=True, null=True)
     host = models.TextField(null=True, blank=True)
 
     def __str__(self) -> str:
@@ -33,6 +34,7 @@ class Channel(models.Model):
     channel_name = models.CharField(max_length=100)
     channel_summary = models.TextField(null=True, blank=True)
     channel_image = models.URLField()
+    channel_twitter = models.URLField(unique=True, null=True)
     host = models.TextField(null=True, blank=True)
 
     def __str__(self) -> str:
