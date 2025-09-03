86ec170e2a27a6e40417b994dd2482724b252d53
David Kotschessa <itzdavey@gmail.com>
2025-09-03 05:42:09 -0400

    created separate settings for production

--- content_aggregator/settings/base.py
+++ content_aggregator/settings/base.py
@@ -26,17 +26,18 @@ load_dotenv()
 # SENTRY
 
 
-sentry_sdk.init(
-    dsn=str(os.getenv("SENTRY_DSN")),
-    integrations=[DjangoIntegration()],
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True,
-)
+# # Production
+# sentry_sdk.init(
+#     dsn=str(os.getenv("SENTRY_DSN")),
+#     integrations=[DjangoIntegration()],
+#     # Set traces_sample_rate to 1.0 to capture 100%
+#     # of transactions for performance monitoring.
+#     # We recommend adjusting this value in production.
+#     traces_sample_rate=1.0,
+#     # If you wish to associate users to errors (assuming you are using
+#     # django.contrib.auth) you may enable sending PII data.
+#     send_default_pii=True,
+# )
 
 from django.urls import path
 
