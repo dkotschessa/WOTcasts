358742f16c4ee4a7abec5a52b6cc8222c5837ca4
David Kotschessa <itzdavey@gmail.com>
2025-09-04 05:24:37 -0400

    if this works I'm confused

--- content_aggregator/settings/ci.py
+++ content_aggregator/settings/ci.py
@@ -1,1 +1,20 @@
 from .base import *
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "whitenoise.runserver_nostatic",
+    "django.contrib.staticfiles",
+    # Local
+    "accounts",
+    "podcasts.apps.PodcastsConfig",
+    "apis.apps.ApisConfig",
+    # Third party apps
+    "django_apscheduler",
+    "django_extensions",
+    "rest_framework",
+    "corsheaders",
+]
