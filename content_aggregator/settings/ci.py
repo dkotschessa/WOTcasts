38e7cd0a9304a1919c7f601a7a9e35d1742f9014
David Kotschessa <itzdavey@gmail.com>
2025-09-04 07:10:35 -0400

    installed apps

--- content_aggregator/settings/ci.py
+++ content_aggregator/settings/ci.py
@@ -1,3 +1,22 @@
 from .base import *
 
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "whitenoise.runserver_nostatic",
+    "django.contrib.staticfiles",
+    # Local
+    "accounts",
+    "podcasts.apps.PodcastsConfig",
+    "apis.apps.ApisConfig",
+    # Third party apps
+    "django_apscheduler",
+    "django_extensions",
+    "rest_framework",
+    "corsheaders",
+]
+
 SECRET_KEY = env("DJANGO_SECRET_KEY")
