d6a9e0ef09b65ceabee3fa8080542bcf7bc49c7e
David Kotschessa <itzdavey@gmail.com>
2025-09-05 16:44:54 -0400

    yamls

--- runtestsmarkdown.sh
+++ runtestsmarkdown.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Running all tests and saving coverage report"
-#pytest -v --md-report  --self-contained-html --cov=.  --cov-report=markdown podcasts accounts
-pytest --cov-report markdown
-echo "test results in testreportdata/report.html"
+pytest --md-report  --self-contained-html --cov=.  podcasts accounts
+coverage report --format=markdown
+#echo "test results in testreportdata/report.html"
