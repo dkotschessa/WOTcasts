ff5248c5cdf1162df8d7facb1ba3d4ba89f1a91e
David Kotschessa <itzdavey@gmail.com>
2025-09-06 09:35:10 -0400

    cleanup

--- runtestsmarkdown.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Running all tests and saving coverage report"
-pytest --md-report  --self-contained-html --cov=.  podcasts accounts
-coverage report --format=markdown
-#echo "test results in testreportdata/report.html"
